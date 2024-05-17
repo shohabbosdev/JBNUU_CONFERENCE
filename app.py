374a07ad3d6350ce769783d746528eb0ffaf14cb
freedom-1 <ushohabbos@gmail.com>
2024-05-17 23:27:23 +0500

    utils.py fayli yasaldi. foydalanuvchilar qismi uchunkodlar optimallashtirildi

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import streamlit as st
-import re
+import pandas as pd
 import json
 import os
-import pandas as pd
-from make_certificate import make_certificates
+from utils import validate_input, make_certificates
 
 st.set_page_config(
     page_title="Sertifikatlarni olish",
@@ -16,100 +15,85 @@ st.set_page_config(
          'About': "# This is a header. This is an *extremely* cool app!"
     })
 
-st.title('Konferensiya ishtirokchilari uchun sertifikat olish')
+st.markdown("""
+<div style='text-align: center;'>
+    <h1>Konferensiya ishtirokchilari uchun sertifikat tayyorlash sahifasi</h1>
+</div>
+""", unsafe_allow_html=True)
 st.markdown("<h3 align='center'><i>Ro'yxatga olish formasi</i></h3>", unsafe_allow_html=True)
 
 st.markdown("---")
-
-# Telefon raqam uchun regex
-phone_regex = r'^\+998\d{9}$'
-
-# Email uchun regex
-email_regex = r'^[\w\.-]+@[\w\.-]+\.\w+$'
 
 directory = "out"
 if not os.path.exists(directory):
     os.makedirs(directory)
 
 with st.form("Birinchi formamiz"):
-   # FISH maydoni
-   fish = st.text_input("F.I.SH", placeholder="Misol: Ulug'murodov Shoh Abbos Baxodir o'g'li", help='Bu maydonda FISH kiritiladi')
+    fish = st.text_input("F.I.SH", placeholder="Misol: Ulug'murodov Shoh Abbos Baxodir o'g'li", help='Bu maydonda FISH kiritiladi')
+    maqola = st.text_input("Maqola mavzuingiz", placeholder="Misol: Ma'lumotlar xavfsizligini oshirishda blockchayn texnologiyalarning o'rni", max_chars=500, help="Bu qismda maqola mavzusi lo'nda kiritilishi kerak")
+    shuba = st.selectbox(
+        "Sho'bani tanlang",
+        options=(
+            "1-sho'ba. Ta'lim tizimini takomillashtirish: dolzarb tendensiyalar va strategik yo'nalishlar",
+            "2-sho'ba. Biomuhandislik va biotexnologiyalar sohasida innovatsiyalar",
+            "3-sho'ba. Raqamli iqtisod va innovatsion axborot-kommunikatsiya texnologiyalarini joriy etishning dolzarb masalalari",
+            "4-sho'ba. Psixologiya, tabiiy va aniq fanlar sohasida dolzarb tadqiqotla",
+            "5-sho'ba. Zamonaviy jamiyatda filologiya, tilshunoslik va didaktika",
+            "6-sho'ba. Zamonaviy gumanitar ta'limning dolzarb muammolari"
+        ),
+        placeholder="O'z sho'bangizni tanlang...", help="O'z sho'bangizni tanlashda qiynalsangiz konferensiya ma'muriyatiga telefon qilishingizi mumkin: https://t.me/UzMU_JF_conf_1_2_3_shuba, https://t.me/UzMU_JF_conf_4_5_6_shuba")
+    email = st.text_input("Email manzilingizni kiriting", placeholder="Misol: ushohabbos@gmail.com", help="Elektron manzil barchangizda bo'ladi degan umiddaman")
+    phone = st.text_input("Telefon raqam", placeholder="+998931189988", help="Telefon raqamni kiriting")
+    rozilik = st.checkbox("Yuqoridagi barcha ma'lumotlar to'g'ri va aniq ekanligini tasdiqlaysizmi?")
+    taqsdiqlash = st.form_submit_button("Tasdiqlash", type='primary')
 
-   # Maqola matnini kiritish
-   maqola = st.text_input("Maqola mavzuingiz", placeholder="Misol: Ma'lumotlar xavfsizligini oshirishda blockchayn texnologiyalarning o'rni", max_chars=500, help="Bu qismda maqola mavzusi lo'nda kiritilishi kerak")
+    if taqsdiqlash:
+        is_valid, error_message = validate_input(fish, email, maqola, phone)
 
-   # Sho'bani tanlash
-   shuba = st.selectbox(
-       "Sho'bani tanlang",
-       options=(
-           "1-sho'ba. Ta'lim tizimini takomillashtirish: dolzarb tendensiyalar va strategik yo'nalishlar",
-           "2-sho'ba. Biomuhandislik va biotexnologiyalar sohasida innovatsiyalar",
-           "3-sho'ba. Raqamli iqtisod va innovatsion axborot-kommunikatsiya texnologiyalarini joriy etishning dolzarb masalalari",
-           "4-sho'ba. Psixologiya, tabiiy va aniq fanlar sohasida dolzarb tadqiqotla",
-           "5-sho'ba. Zamonaviy jamiyatda filologiya, tilshunoslik va didaktika",
-           "6-sho'ba. Zamonaviy gumanitar ta'limning dolzarb muammolari"
-       ),
-       placeholder="O'z sho'bangizni tanlang...", help="O'z sho'bangizni tanlashda qiynalsangiz konferensiya ma'muriyatiga telefon qilishingizi mumkin: https://t.me/UzMU_JF_conf_1_2_3_shuba, https://t.me/UzMU_JF_conf_4_5_6_shuba")
+        if is_valid:
+            st.success("Ma'lumotlar muvaffaqiyatli saqlandi", icon='✅')
 
-   # 2 ta columnga bo'lamiz
-   col1, col2 = st.columns(2)
+            table = {
+                "F.I.SH": fish,
+                "Maqola mavzusi": maqola,
+                "Sho'bani nomi": shuba,
+                "Email manzil": email,
+                "Telefon raqam": phone
+            }
+            with st.expander("Siz kiritgan ma'lumotlar bilan tanishing👇👇👇"):
+                st.table(table)
 
-   # Email manzilingizni kiritish
-   email = col1.text_input("Email manzilingizni kiriting", placeholder="Misol: ushohabbos@gmail.com", help="Elektron manzil barchangizda bo'ladi degan umiddaman")
+            try:
+                with open('out/data.json', 'r') as f:
+                    existing_data = json.load(f)
+            except (FileNotFoundError, json.decoder.JSONDecodeError):
+                existing_data = []
 
-   # Telefon raqamni kiritish
-   phone = col2.text_input("Telefon raqam", placeholder="+998931189988", help="Telefon raqamni kiriting")
+            df = pd.DataFrame(existing_data)
 
-   # Kiritilgan ma'lumotlar to'g'ri ekanligiga ishonish
-   rozilik = st.checkbox("Yuqoridagi barcha ma'lumotlar to'g'ri va aniq ekanligini tasdiqlaysizmi?")
+            existing_emails = df['Email manzil'].tolist()
+            existing_phones = df['Telefon raqam'].tolist()
+            existing_fish = df['F.I.SH'].tolist()
+            existing_maqola = df['Maqola mavzusi'].tolist()
 
-   # Ma'lumotlarni saqlash
-   taqsdiqlash = st.form_submit_button("Tasdiqlash", type='primary')
+            if email in existing_emails or phone in existing_phones or fish in existing_fish or maqola in existing_maqola:
+                st.warning(f"Siz ro'yxattan o'tgansiz! Pastda siz olgan sertifikat nusxasi mavjud", icon='⚠️')
+                with st.expander("Sertifikatingiz👉👉👉"):
+                    st.image(f"out/{fish}.png", caption='Bu sizning sertifikatingiz')
+            else:
+                existing_data.append(table)
+                df = pd.DataFrame(existing_data)
+                with open('out/data.json', 'w') as f:
+                    json.dump(existing_data, f, indent=4)
 
-   # Shartlar
-   if rozilik and taqsdiqlash:
-       if fish == "" or email == "" or maqola == "":
-           st.warning("Iltimos barcha maydonlar bo'sh bo'lmasligini ta'minlang!!", icon='⚠️')
-       elif not re.match(phone_regex, phone):
-           st.warning("Telefon raqam noto'g'ri formatda kiritildi. Iltimos, +998xxxxxxxx formatida kiriting.", icon='⚠️')
-       elif not re.match(email_regex, email):
-           st.warning("Email manzil noto'g'ri formatda kiritildi. Iltimos, to'g'ri formatda kiriting.", icon='⚠️')
-       else:
-           st.success("Ma'lumotlar muvaffaqiyatli saqlandi", icon='✅')
+                make_certificates(fish, maqola)
+                st.image(f"out/{fish}.png", caption='Sertifikatingizni yuklab olishingiz mumkin')
 
-           # Ma'lumotlarni ekranga chiqarish
-           table = {
-               "F.I.SH": fish,
-               "Maqola mavzusi": maqola,
-               "Sho'bani nomi": shuba,
-               "Email manzil": email,
-               "Telefon raqam": phone
-           }
-           st.table(table)
-           
-           # Ma'lumotlarni data.json fayliga saqlash
-           try:
-               with open('out/data.json', 'r') as f:
-                   existing_data = json.load(f)
-           except (FileNotFoundError, json.decoder.JSONDecodeError):
-               existing_data = []
+            # Barcha ro'yxatdan o'tganlar ma'lumotlarini ko'rsatish
+            with st.expander("Barcha ro'yxatdan o'tganlar ma'lumotlari"):
+                # Telefon raqamlarni qisqartirib olish
+                df['Telefon raqam'] = df['Telefon raqam'].apply(lambda x: f"+998***{x[-4:]}")
+                st.dataframe(df)
 
-           existing_emails = [entry['Email manzil'] for entry in existing_data]
-           existing_phones = [entry['Telefon raqam'] for entry in existing_data]
-           existing_fish   = [entry['F.I.SH'] for entry in existing_data]
-           existing_maqola = [entry['Maqola mavzusi'] for entry in existing_data]
-
-           if email in existing_emails or phone in existing_phones or fish in existing_fish or maqola in existing_maqola:
-               st.warning(f"Siz ro'yxattan o'tgansiz! Sizga sertifikat bera olmaymiz", icon='⚠️')
-               st.image(f"out/{fish}.png",caption='Bu sizning sertifikatingiz')
-           else:
-               existing_data.append(table)
-               with open('out/data.json', 'w') as f:
-                   json.dump(existing_data, f, indent=4)
-
-               # Make certificate
-               make_certificates(fish, maqola)
-               st.image(f"out/{fish}.png",caption='Sertifikatingizni yuklab olishingiz mumkin')
-
-   elif taqsdiqlash and not rozilik:
-       st.error("Siz ma'lumotlaringiz to'g'ri ekanlgini tasdiqlashingiz kerak!!", icon='❌')
+        else:
+            st.error(f"Xatolik nomi: {error_message}", icon='❌')
--- utils.py
+++ utils.py
@@ -1,19 +1,36 @@
 from PIL import Image, ImageFont, ImageDraw
 import os
+import re
+
+# Telefon raqam uchun regex
+phone_regex = r'^\+998\d{9}$'
+
+# Email uchun regex
+email_regex = r'^[\w\.-]+@[\w\.-]+\.\w+$'
+
+def validate_input(fish, email, maqola, phone):
+    if fish == "" or email == "" or maqola == "":
+        return False, "Iltimos barcha maydonlar bo'sh bo'lmasligini ta'minlang!!"
+    elif not re.match(phone_regex, phone):
+        return False, "Telefon raqam noto'g'ri formatda kiritildi. Iltimos, +998xxxxxxxx formatida kiriting."
+    elif not re.match(email_regex, email):
+        return False, "Email manzil noto'g'ri formatda kiritildi. Iltimos, to'g'ri formatda kiriting."
+    else:
+        return True, None
+
 # Global Variables
 FONT_FILE_1 = ImageFont.truetype(r'fonts/Times New Roman Bold.ttf', 30)
 FONT_FILE_2 = ImageFont.truetype(r'fonts/Times New Roman Bold.ttf', 14)
 FONT_COLOR_1 = "#5E17EB"
 FONT_COLOR_2 = "#0E477D"
-
-template = Image.open(r'src/Sertifikat.png')
-WIDTH, HEIGHT = template.size
-
+TEMPLATE_IMAGE = Image.open(r'src/Sertifikat.png')
+WIDTH, HEIGHT = TEMPLATE_IMAGE.size
 MAX_WIDTH = WIDTH - 80  # Ikkinchi matn uchun maksimal eni
 MAX_WORDS_PER_LINE = 8  # Har bir qatorda maksimal so'z soni
+OUTPUT_DIR = "out"
 
 def make_certificates(name, second_text):
-    
+    template = TEMPLATE_IMAGE.copy()
     draw = ImageDraw.Draw(template)
 
     # Birinchi matnning eni va balandligini topish
@@ -29,7 +46,7 @@ def make_certificates(name, second_text):
     lines = []
     line = []
     for word in words:
-        if len(line) >= MAX_WORDS_PER_LINE:
+        if len(' '.join(line + [word])) > MAX_WIDTH:
             lines.append(' '.join(line))
             line = []
         line.append(word)
@@ -37,7 +54,6 @@ def make_certificates(name, second_text):
         lines.append(' '.join(line))
 
     y = (HEIGHT - text_height_1) / 2 + 55 + text_height_1 + 10
-
     for line in lines:
         bbox_2 = FONT_FILE_2.getbbox(line)
         text_width_2 = bbox_2[2] - bbox_2[0]
@@ -46,7 +62,7 @@ def make_certificates(name, second_text):
         y += text_height_2 + 5  # Qatorlar orasidagi masofani oshirish
 
     # Sertifikatlarni boshqa katalogga saqlash
-    directory = "out"
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-    template.save("./out/" + name + ".png")
+    if not os.path.exists(OUTPUT_DIR):
+        os.makedirs(OUTPUT_DIR)
+
+    template.save(f"{OUTPUT_DIR}/{name}.png")
